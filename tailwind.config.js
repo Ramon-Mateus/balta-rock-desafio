7b1e5f6cfeaa80ec01e028c26dbd678f45e64929
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-07-17 13:36:49 -0300

    feat: navBar struct

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'roxo-balta': 'rgba(134, 37, 210, 1)',
+      },
+    },
   },
   plugins: [],
 }
-
