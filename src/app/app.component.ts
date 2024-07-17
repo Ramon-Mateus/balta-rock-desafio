bad546c71a2f904ce9b1c2ecde363ff511586d29
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-07-17 15:53:41 -0300

    feat: ui search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [
+    RouterOutlet,
+    NavBarComponent,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  
+  eventSearch: string = '';
+
+  searchEvents(): void {
+    if(this.eventSearch.trim()) {
+      console.log(this.eventSearch)
+    }
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     extend: {
       colors: {
         'roxo-balta': 'rgba(134, 37, 210, 1)',
+        'cinza-navbar-l': 'rgba(231, 232, 228, 1)'
       },
     },
   },
