bc2322413146ce218f02f22516327e6c0afa32cb
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-07-19 14:55:01 -0300

    Ajustes

--- src/app/components/new-event/new-event.component.ts
+++ src/app/components/new-event/new-event.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { Show } from '../../types';
 import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { ShowService } from '../../services/show.service';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-new-event',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, CalendarModule],
   templateUrl: './new-event.component.html',
   styleUrl: './new-event.component.css'
 })
@@ -16,5 +17,6 @@ export class NewEventComponent {
   submitted = false;
   onSubmit() {
     this.showService.createShow(this.model)
+    this.model = {'name': '', 'local': '', 'date': '', 'adress': ''}
   }
 }
